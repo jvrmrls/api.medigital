05503c262a5ae031eb06b04c524ac42edfdcff42
Javier Morales <javiermoralesmelara@gmail.com>
2022-10-24 23:10:09 -0600

    CHANGES: Validation

--- src/controllers/DateController.js
+++ src/controllers/DateController.js
@@ -25,7 +25,7 @@ router.post(
   '/',
   authenticateToken,
   body('name').notEmpty().withMessage('El nombre no es válido'),
-  body('date').isDate().withMessage('La fecha no es válida'),
+  body('date').isISO8601().withMessage('La fecha no es válida'),
   body('hour')
     .matches(new RegExp(/^(10|11|12|[1-9]):[0-5][0-9]$/))
     .withMessage('La hora no es válida'),
@@ -38,9 +38,9 @@ router.put(
   authenticateToken,
   param('_id').isMongoId().withMessage('El ID no es del formato correcto'),
   body('name').notEmpty().withMessage('El nombre no es válido'),
-  body('date').isDate().withMessage('La fecha no es válida'),
+  body('date').isISO8601().withMessage('La fecha no es válida'),
   body('hour')
-    .matches(new RegExp(/^(10|11|12|[1-9]):[0-5][0-9]$/))
+    .matches(new RegExp(/^([0-2][1-9]):[0-5][0-9]$/))
     .withMessage('La hora no es válida'),
   body('reason').notEmpty().withMessage('La razón no es válida'),
   update
