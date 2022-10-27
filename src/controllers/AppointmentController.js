b5fa6d5665f638c03d3080fc33f8385995103244
Javier Morales <javiermoralesmelara@gmail.com>
2022-10-26 20:04:37 -0600

    FIXED BUGS: Matches

--- src/controllers/AppointmentController.js
+++ src/controllers/AppointmentController.js
@@ -27,7 +27,7 @@ router.post(
   body('name').notEmpty().withMessage('El nombre no es válido'),
   body('date').isISO8601().withMessage('La fecha no es válida'),
   body('hour')
-    .matches(new RegExp(/^(10|11|12|[1-9]):[0-5][0-9]$/))
+    .matches(/^(([0-9][0-2]?)|([0][0-9])):[0-5][0-9]$/)
     .withMessage('La hora no es válida'),
   body('reason').notEmpty().withMessage('La razón no es válida'),
   create
@@ -40,7 +40,7 @@ router.put(
   body('name').notEmpty().withMessage('El nombre no es válido'),
   body('date').isISO8601().withMessage('La fecha no es válida'),
   body('hour')
-    .matches(new RegExp(/^([0-2][1-9]):[0-5][0-9]$/))
+    .matches(/^(([0-9][0-2]?)|([0][0-9])):[0-5][0-9]$/)
     .withMessage('La hora no es válida'),
   body('reason').notEmpty().withMessage('La razón no es válida'),
   update
