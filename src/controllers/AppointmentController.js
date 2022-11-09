68d2f4e33c323f699ed1bc55df2dc8ac63e79c6f
Javier Morales <javiermoralesmelara@gmail.com>
2022-11-08 20:12:17 -0600

    Update AppointmentController.js

--- src/controllers/AppointmentController.js
+++ src/controllers/AppointmentController.js
@@ -50,9 +50,7 @@ router.put(
   param('_id').isMongoId().withMessage('El ID no es del formato correcto'),
   body('name').notEmpty().withMessage('El nombre no es válido'),
   body('date').isISO8601().withMessage('La fecha no es válida'),
-  body('hour')
-    .matches(/^(([0-9][0-2]?)|([0][0-9])):[0-5][0-9]$/)
-    .withMessage('La hora no es válida'),
+  body('hour').notEmpty().withMessage('La hora no es válida'),
   body('reason').notEmpty().withMessage('La razón no es válida'),
   update
 )
