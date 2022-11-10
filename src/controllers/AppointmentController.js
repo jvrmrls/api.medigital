e335b47869a487edfa50f58fa480d037809c45ee
Javier Morales <javiermoralesmelara@gmail.com>
2022-11-09 21:33:43 -0600

    Update AppointmentController.js

--- src/controllers/AppointmentController.js
+++ src/controllers/AppointmentController.js
@@ -37,9 +37,7 @@ router.post(
   authenticateToken,
   body('name').notEmpty().withMessage('El nombre no es válido'),
   body('date').isISO8601().withMessage('La fecha no es válida'),
-  body('hour')
-    .matches(/^(([0-9][0-2]?)|([0][0-9])):[0-5][0-9]$/)
-    .withMessage('La hora no es válida'),
+  body('hour').notEmpty().withMessage('La hora no es válida'),
   body('reason').notEmpty().withMessage('La razón no es válida'),
   create
 )
